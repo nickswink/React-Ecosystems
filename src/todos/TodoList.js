9cff36a52d55a2b15ebf994ad9bd69ac90aa165d
Nick Swink <nickswink7@gmail.com>
2020-11-14 15:15:00 -0500

    added redux thunk for side effect async funcs

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import NewTodoForm from './NewTodoForm';
 import TodoListItem from './TodoListItem';
+import { displayAlert, loadTodos } from './thunk';
 import { completeTodo, removeTodo } from './actions';
 import { connect } from 'react-redux';
 import './TodoList.css'
 
-const TodoList = ({ todos = [], onRemovePressed, onCompletePressed }) => {
-    return(
-        <>
+const TodoList = ({ todos = [], onRemovePressed, onCompletePressed, isLoading, startLoadingTodos }) => {
+    useEffect(() => {
+        startLoadingTodos();
+    }, []);
+
+    const loadingMessage = <div>Loading todos...</div>;
+    const content = (
         <div className="list-wrapper">
             <NewTodoForm />
             {todos.map(todo => <TodoListItem todo={todo} onRemovePressed={onRemovePressed} onCompletePressed={onCompletePressed}/>)}
         </div>
-        </>
     )
+    return isLoading ? loadingMessage : content;
 };
 
 const mapStateToProps = (state) => ({
+    isLoading: state.isLoading,
     todos: state.todos,
 });
 
 const mapDispatchToProps = dispatch => ({
+    startLoadingTodos: () => dispatch(loadTodos),
     onRemovePressed: text => dispatch(removeTodo(text)),
     onCompletePressed: text => dispatch(completeTodo(text)),
 });
--- src/todos/reducers.js
+++ src/todos/reducers.js
@@ -1,4 +1,21 @@
-import {COMPLETE_TODO, CREATE_TODO, REMOVE_TODO} from './actions';
+import {COMPLETE_TODO, CREATE_TODO, LOAD_TODOS_FAILURE, LOAD_TODOS_SUCCESS, REMOVE_TODO} from './actions';
+
+
+export const isLoading = (state = false, action) => {
+    const { type } = action;
+    switch (type) {
+        case LOAD_TODOS_IN_PROGRESS:
+            return true;
+        case LOAD_TODOS_SUCCESS:
+        case LOAD_TODOS_FAILURE:
+            return false;
+        default:
+            return state;
+    }
+}
+
+
+
 
 export const todos = (state = [], action) => {
     const { type, payload } = action;
